994421b1b3b274aa6d80895f4da9b7bb825dd18f
landrykhamin <landryingabire605@gmail.com>
2025-09-25 20:11:04 +0200

    we added conditional file

---
+++ conditional.py
@@ -0,0 +1,25 @@
+#Boolean Logic
+#True or False
+
+# data = 3 + 5
+# data = 3 < 5
+
+# print(type(data))
+# print(data)
+
+# number = 20
+
+# if number == 10:
+#     print("Number is 10")
+
+# print("End of the program")
+
+print("Enter you name:")
+
+name = input()  #input function takes input from user
+
+if name == "Landry":
+    print("Welcome Landry")
+
+else:
+    print("You are not User, Please register!")
